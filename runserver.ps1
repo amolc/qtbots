03fafee20b3b50eb7ef2577a5ca8d3cae1ebb066
amolc <amol.c@fountaintechies.com>
2023-02-27 20:47:58 +0800

    added the settings

--- runserver.ps1
+++ runserver.ps1
@@ -1,4 +1,10 @@
 .\venv\Scripts\activate
+
+set PORT="7000"
+set APP_SEC="f6cd13f489fc06655b9e262363eccaff"
+set DEF_USER="aayushrrverma@gmail.com"
+set DEF_PASS="9926094939Aa!"
+
 .\settings.sh
 pip install -r requirements.txt
 
